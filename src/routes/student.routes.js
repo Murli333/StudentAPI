e01bc79b8d6d45ad654decf50adffe4aff837f55
Murli333 <2k24.csai1b.2413183@gmail.com>
2026-07-25 22:33:53 +0530

    Finalisation

--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -1,6 +1,35 @@
-const {get} = require("../controllers/student.controller");
 const express = require("express");
 const router = express.Router();
-router.get("/students",get);
 
-module.exports = {router};
+const {
+     welcome,
+    about,
+    studentInfo,
+    info,
+    getStudents,
+    getStudentById,
+    createStudent,
+    deleteStudent,
+    updateStudent
+} = require("../controllers/student.controller");
+
+router.get("/",welcome);
+
+router.get("/about",about);
+
+router.get("/student",studentInfo);
+
+router.get("/info",info);
+
+router.get("/students",getStudents);
+
+router.get("/students/:i",getStudentById);
+
+router.post("/students",createStudent);
+
+router.post("/students/:id",deleteStudent);
+
+router.put("/students/:id",updateStudent);
+
+module.exports = router;
+
